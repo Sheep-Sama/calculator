45c744a908b8e6b5e8aa5e1044aa621c67710149
The-Sheep-Noob <yohann.vdh2008@gmail.com>
2023-07-15 17:29:53 +0200

    added error checking for user input

--- calc.asm
+++ calc.asm
@@ -6,10 +6,10 @@ section .bss
 
 section .data
     closing_angle_braquet db "> "
-    error_message_cro db "Error (1): couldn't resolve opperation"
+    error_message_cro db "Error (1): couldn't resolve opperation",10
     exit_cmd db "exit"
     exit_cmd2 db "quit"
-
+    for_testing db "opperation succeed !",10
 
 section .text
 global _start
@@ -17,9 +17,16 @@ _start:
     call _prompt
     call _read_user_input
     call _check_exit
+    call _check_invalide_char
+    cmp r8 , 1
+    je .restart
 
-    ; calculation and output
+    mov rax , 1
+    mov rdi , 1
+    mov rsi , for_testing
+    mov rdx , 21
+    syscall
+    ; calculation and output goes here
 
+.restart:
     jmp _start
-    .exit:
-    exit
